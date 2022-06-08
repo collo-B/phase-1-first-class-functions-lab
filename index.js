831e741017509d6bccd728f1ac8cb858821c9ed4
BII KIPKOECH COLLINS <ckipkoech17@gmail.com>
2022-06-08 08:25:32 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers=function(){
+return(drivers.slice(0,2))
+}
+
+const returnLastTwoDrivers=function(){
+return(drivers.slice(-2))
+}
+
+const selectingDrivers =[returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier=function(value){
+ return function(quad){
+     return(value*quad)
+ }
+}
+
+const fareDoubler=function(fare){
+return(fare*2)
+}
+const fareTripler=function(tripleFare){
+        return(tripleFare*3)
+}
+const selectDifferentDrivers=function(drivers , returnDrivers){
+return returnDrivers(drivers)
+}
